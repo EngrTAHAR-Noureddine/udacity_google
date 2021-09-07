d02afeae7dceeb11b66c08374e8889594ce02744
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-09-07 18:11:39 +0100

    add checkbox , add some styling , add intent mail

--- second_lesson_inputUI/app/src/main/java/com/example/second_lesson/MainActivity.java
+++ second_lesson_inputUI/app/src/main/java/com/example/second_lesson/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.second_lesson;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -15,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     int numberCoffees =0;
+    boolean hasCream = false;
+    boolean hasChocolate =false;
+    String hasName = "Name";
     public void incrementQuantity(View view){
         numberCoffees++;
         display(numberCoffees);
@@ -23,21 +30,50 @@ public class MainActivity extends AppCompatActivity {
         if(numberCoffees>0)numberCoffees--;
         display(numberCoffees);
     }
-    public void submitOrder(View view){
-
-        displayPrice(numberCoffees * 20);
-    }
     private void display(int number){
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText(""+number);
     }
+
+    public void submitOrder(View view){
+        CheckBox checkBox = (CheckBox) findViewById(R.id.notify_me_checkbox);
+        hasCream = checkBox.isChecked();
+        checkBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        hasChocolate = checkBox.isChecked();
+        EditText editText = (EditText) findViewById(R.id.album_description_view);
+        hasName = editText.getText().toString();
+
+        displayPrice(numberCoffees * 20);
+    }
+
     /**
      * This method displays the given price on the screen.
      */
 
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
+        String name = "Name : ";
+        if(hasName!=null) name+=hasName; else name+="from App";
+        String text = name+"\n";
+         text += String.format("Quantity : %d \n", numberCoffees);
+        text += "Whipped cream :";
+        if(hasCream) text += " YES\n"; else text += " NO\n";
+        text += "Chocolate :";
+        if(hasChocolate) text += " YES\n"; else text += " NO\n";
+        text += "Total : "+NumberFormat.getCurrencyInstance().format(number)+"\n";
 
+
+        priceTextView.setText(text);
+        composeEmail(name,text);
+    }
+    public void composeEmail( String subject,String body) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        //intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, body);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 }
